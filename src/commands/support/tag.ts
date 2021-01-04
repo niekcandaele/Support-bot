21fbff6193a2dfe1b5d7a9c77afb160827fc306c
Niek Candaele <niekcandaele@gmail.com>
2021-01-04 22:48:16 +0100

    Add support for CSMM and CPM commands as tags

--- src/commands/support/tag.ts
+++ src/commands/support/tag.ts
@@ -1,6 +1,8 @@
 import { Command, CommandoClient, CommandoMessage } from 'discord.js-commando';
 import { Message, MessageEmbed } from 'discord.js';
 import getTags from '../../tagsCache';
+import { Tag as ITag } from '../../tagsCache';
+import getCommands from '../../commandsCache';
 
 
 export default class Tag extends Command {
@@ -16,8 +18,7 @@ export default class Tag extends Command {
 
     async run(message: CommandoMessage, args: string): Promise<Message | Message[]> {
         const tags = await getTags();
-
-        const tagToSend = tags.filter(t => t.code).find(t => t.code.toLowerCase() === args.toLowerCase());
+        const tagToSend = await determineReply(args);
 
         if (!tagToSend) {
             const possibleTags = tags.filter(t => t.code).map(t => `\`${t.code}\``).join(', ')
@@ -32,3 +33,38 @@ export default class Tag extends Command {
     }
 }
 
+async function determineReply(textToSearch): Promise<ITag> {
+    // First try to match with a tag
+    const tags = await getTags();
+
+    const tagToSend = tags.filter(t => t.code).find(t => t.code.toLowerCase() === textToSearch.toLowerCase());
+
+    if (tagToSend) {
+        return tagToSend;
+    }
+
+    // If no tags were found, we try to match with a command
+    const commands = await getCommands();
+
+    const commandToSend = commands.find(c => {
+        return c.name.toLowerCase() === textToSearch.toLowerCase() || c.aliases.find(a => a.toLowerCase() === textToSearch.toLowerCase())
+    });
+
+    if (commandToSend) {
+        const data: ITag = {
+            response: `
+            ${commandToSend.name}
+
+            ${commandToSend.description}
+
+            ${commandToSend.extendedDescription}`,
+            code: '',
+            trigger: ''
+        }
+
+        return data;
+    }
+
+    // Found nothing 😭
+    return null
+}
---
+++ src/commandsCache.test.ts
@@ -0,0 +1,22 @@
+import NodeCache from "node-cache";
+// Start spying before the cache is initialized in the function 
+const setSpy = jest.spyOn(NodeCache.prototype, 'set');
+import getCommands from "./commandsCache";
+
+
+describe('Tags cache', function () {
+
+    it('Returns commands', async function () {
+        const res = await getCommands();
+        expect(res).toBeInstanceOf(Array)
+    })
+
+    it('Caches commands', async function () {
+        const res = await getCommands();
+        expect(setSpy).toBeCalledTimes(1);
+        expect(res).toBeInstanceOf(Array)
+        await getCommands();
+        expect(setSpy).toBeCalledTimes(1);
+
+    })
+})

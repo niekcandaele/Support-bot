fcebf3072b28e7d745d37069b8f3fa71506da38c
Niek Candaele <niek@candaele.dev>
2021-12-11 17:26:54 +0100

    feat: implement levenshtein distance calculation to allow easier tag matching when typo'ing

--- src/commands/support/tag.ts
+++ src/commands/support/tag.ts
@@ -1,5 +1,6 @@
 import { Message, MessageEmbed } from "discord.js";
 import { Command, CommandoClient, CommandoMessage } from "discord.js-commando";
+import leven from "leven";
 
 import getCommands from "../../commandsCache";
 import { Tag as ITag, tags } from "../../tags";
@@ -34,47 +35,69 @@ export default class Tag extends Command {
 
     const embed = new MessageEmbed();
 
-    embed.setDescription(tagToSend.response);
+    embed.setDescription(tagToSend.tag.response);
+    embed.setFooter(
+      `Type: ${tagToSend.type} - Trigger: ${tagToSend.tag.code} - Levenshtein distance: ${tagToSend.distance}`
+    );
 
     return message.channel.send(embed);
   }
 }
 
-async function determineReply(textToSearch): Promise<ITag> {
+async function determineReply(
+  textToSearch: string
+): Promise<{ tag: ITag; type: string; distance: number }> {
   const tagToSend = tags
     .filter((t) => t.code)
-    .find((t) => t.code.toLowerCase() === textToSearch.toLowerCase());
-
-  if (tagToSend) {
-    return tagToSend;
-  }
+    .map((t) => {
+      return {
+        ...t,
+        score: leven(t.code.toLowerCase(), textToSearch.toLowerCase()),
+      };
+    })
+    .filter((t) => t.score < 3)
+    .map((t) => {
+      return { tag: t, type: "support tag", distance: t.score };
+    });
 
   // If no tags were found, we try to match with a command
   const commands = await getCommands();
 
-  const commandToSend = commands.find((c) => {
-    return (
-      c.name.toLowerCase() === textToSearch.toLowerCase() ||
-      c.aliases.find((a) => a.toLowerCase() === textToSearch.toLowerCase())
-    );
-  });
+  const commandToSend = commands
+    .filter((c) => c.name)
+    .map((c) => {
+      return {
+        ...c,
+        scoreName: leven(c.name.toLowerCase(), textToSearch.toLowerCase()),
+        scoreAliases: c.aliases.length
+          ? Math.min(
+              ...c.aliases.map((a) =>
+                leven(a.toLowerCase(), textToSearch.toLowerCase())
+              )
+            )
+          : 99999,
+      };
+    })
+    .map((c) => {
+      return {
+        ...c,
+        score: Math.min(c.scoreName, c.scoreAliases) * c.priority,
+      };
+    })
+    .filter((c) => c.scoreName < 3 || c.scoreAliases < 3)
+    .map((c) => {
+      const data: ITag = {
+        response: `${c.name}\n\n${c.description}\n\n${c.extendedDescription}`,
+        code: c.name,
+      };
+      return { tag: data, type: "command", distance: c.score };
+    });
 
-  if (commandToSend) {
-    const data: ITag = {
-      response: `
-            ${commandToSend.name}
+  const matches = commandToSend
+    .concat(tagToSend)
+    .sort((a, b) => a.distance - b.distance);
 
-            ${commandToSend.description}
-
-            ${commandToSend.extendedDescription}`,
-      code: "",
-    };
-
-    return data;
-  }
-
-  // Found nothing 😭
-  return null;
+  return matches[0];
 }
 
 function alphabeticalComparer(a: string, b: string): number {
